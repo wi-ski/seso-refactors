2d4c6aa4c5642e2bfb310b82f8c9162fcd02a124
Will Dembinski <willdembinski@gmail.com>
2023-02-05 20:29:35 -0800

    Cleanup

--- src/extract-function/provider-templates/buildBarrelExportBetaDomainRoot.ts
+++ src/extract-function/provider-templates/buildBarrelExportBetaDomainRoot.ts
@@ -8,12 +8,8 @@ export const buildBarrelExportBetaDomainRoot: TSourceFileConfiguratorFn = (
   if (p.writeFileConfigs.gammaDomain) {
     const exportStatement = `export * as ${p.writeFileConfigs.gammaDomain} from "./${p.writeFileConfigs.gammaDomain}";`;
     const statements = p.sourcefileConfig.sourceFile.getStatements();
-    console.log({ exportStatement });
-    console.log({ exportStatement });
-    console.log({ exportStatement });
-    console.log({ exportStatement });
+
     const containsExportAlready = statements.some((s) => {
-      console.log({ SSSSSS: s });
       return s.getText() === exportStatement;
     });
     if (containsExportAlready) {
