1efcc564c7142eb47794eaee7518a07d95642d56
Will Dembinski <willdembinski@gmail.com>
2023-02-05 18:21:53 -0800

    Rename provider types

--- src/extract-function/provider-templates/buildProviderApplicationEventListener.ts
+++ src/extract-function/provider-templates/buildProviderApplicationEventListener.ts
@@ -8,7 +8,7 @@ import type * as TSeso from "@/lib/types";
 export type ${"TEMPLATE_PARAMS_TYPE_NAME"} = ${"TEMPLATE_PARAMS_TYPE"};
 export type ${"TEMPLATE_RESPONSE_TYPE_NAME"} = ${"TEMPLATE_RESPONSE_TYPE"};
 
-export const ${"TEMPLATE_PROVIDER_NAME"}: TSeso.TDDD.domain.TDomainService<${"TEMPLATE_PARAMS_TYPE_NAME"}, ${"TEMPLATE_RESPONSE_TYPE_NAME"}> = (
+export const ${"TEMPLATE_PROVIDER_NAME"}: TSeso.TDDD.application.TApplicationEventListener<${"TEMPLATE_PARAMS_TYPE_NAME"}, ${"TEMPLATE_RESPONSE_TYPE_NAME"}> = (
   $r,
   ${"TEMPLATE_PARAMS_BLOB"}
 ) => {
